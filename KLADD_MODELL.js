bd4385a7342d19152453e6c72e1299ba0a20d4d4
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-10-15 15:08:25 +0200

    x

---
+++ KLADD_MODELL.js
@@ -0,0 +1,35 @@
+// pets:
+// [
+//     { id: 1, name: 'owl', description: '+1 atk, +1 def, +1 spd', atk: 1, def: 1, spd: 1 },
+//     { id: 2, name: 'horse', description: '+2 atk, +2 def, +2 spd', atk: 2, def: 2, spd: 2 },
+//     { id: 3, name: 'dragon', description: '+3 atk, +3 def, +3 spd', atk: 3, def: 3, spd: 3 },
+// ],
+
+// quests:
+// [
+//     { id: 1, area: 'town', questName: 'Quest1', killCount: 0, questStep: 0 },
+//     { id: 2, area: 'cave', questName: 'Quest2', killCount: 0, questStep: 0 },
+//     { id: 3, area: 'desert', questName: 'Quest3', killCount: 0, questStep: 0 },
+//     { id: 4, area: 'forest', questName: 'Quest4', killCount: 0, questStep: 0 },
+//     { id: 5, area: 'mountains', questName: 'Quest5', killCount: 0, questStep: 0 },
+// ],
+
+// boss:
+// [
+//     { id: 1, boss: 'town boss', bossName: 'Big Boss', bossStep: 0 },
+//     { id: 2, boss: 'cave boss', bossName: 'Small Boss', bossStep: 0 },
+//     { id: 3, boss: 'desert boss', bossName: 'Boss', bossStep: 0 },
+//     { id: 4, boss: 'forest boss', bossName: 'Middle Boss', bossStep: 0 },
+//     { id: 5, boss: 'mountains boss', bossName: 'Small Boss', bossStep: 0 },
+// ],
+
+// raids:
+// [
+//     { id: 1, boss: 'Raids Boss', BossName: 'Main Boss', bossSted: 0, hasKey: false },
+// ],
+
+// items:
+//     [
+//         { id: 1, name: 'Life Potion', description: 'heals 20 Hp' },
+//         { id: 2, name: 'Mana Potion', description: 'heals 20 Mp' },
+//     ],
