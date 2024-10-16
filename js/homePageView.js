094df61bac96d9a89dc0a19e491ea4f9d66de859
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-10-16 14:56:38 +0200

    x

--- js/homePageView.js
+++ js/homePageView.js
@@ -2,11 +2,13 @@ function homePageView() {
     model.app.currentPage = 'homePage';
 
     document.getElementById('app').innerHTML = /*HTML*/ `
-            <div class="homePage">
-            <h1> World Of Clickcraft</h1>
-                <button onclick="goToLoginPage()">Log in</button>
-                <br/>
+        <div class="homePage">
+            <img src="../img/WoF.png" alt="World of Clickcraft logo" class="wof-logo">
+            <h1>World Of Clickcraft</h1>
+            <div>
+                <button onclick="goToLoginPage()">Login</button>
                 <button onclick="goToRegisterPage()">Register</button>
             </div>
+        </div>
     `;
 }
