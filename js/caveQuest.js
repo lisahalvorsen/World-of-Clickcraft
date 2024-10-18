ffe1c7346dcc3b8dc020b492c9e83643184e7e19
IverHK <iverhk@gmail.com>
2024-10-18 14:21:02 +0200

    d

--- js/caveQuest.js
+++ js/caveQuest.js
@@ -6,20 +6,25 @@ function caveQuest(){
 }
 
 function attackCaveMonster(){
-	monsterCurrentHp = model.caveQuest.currentHp;
-	characterCurrentHp = model.characters.some(character => character.userId === loggedInCharacterId)
-	// characterAttLvl = 
-	// characterDefLvl = 
-	// monsterAttLvl = 
-	// monsterDefLvl = 
+	let characterStats = model.stats.find(stat => stat.characterId === model.app.loggedInCharacterId);
+	let characterCurrentHp = characterStats ? characterStats.currenthp : null;
+	let characterAtk = characterStats.atk;
+	let characterDef = characterStats.def;
+	
+	let caveMonsterStats = model.caveQuest[1];
+	let caveMonsterCurrentHp = caveMonsterStats.currenthp;
+	let caveMonsterAtk = caveMonsterStats.atk;
+	let caveMonsterDef = caveMonsterStats.def;
 
+	return (
+	console.log(`damage taken on cave monster ${calculateDamage(characterAtk, caveMonsterDef)}`))
 
 }
 
-function calculateDamage(characterAttLvl, monsterDefLvl) {
+function calculateDamage(atkLvl, defLvl) {
     let maxDamage = 100;  
-    let userMaxDamage = (characterAttLvl / 100) * maxDamage;
-    let actualDamage = (monsterDefLvl / 100) * userMaxDamage;
+    let userMaxDamage = (atkLvl / 100) * maxDamage;
+    let actualDamage = (defLvl / 100) * userMaxDamage;
     let finalDamage = Math.floor(Math.random() * actualDamage);
 
     return finalDamage;
