5b15cf5b575a3a995be719adad5fe9c7a5ff3fd3
IverHK <iverhk@gmail.com>
2024-10-18 13:50:14 +0200

    added cavequest to index.html

--- js/caveQuest.js
+++ js/caveQuest.js
@@ -8,8 +8,20 @@ function caveQuest(){
 function attackCaveMonster(){
 	monsterCurrentHp = model.caveQuest.currentHp;
 	characterCurrentHp = model.characters.some(character => character.userId === loggedInCharacterId)
+	// characterAttLvl = 
+	// characterDefLvl = 
+	// monsterAttLvl = 
+	// monsterDefLvl = 
 
-	
 
+}
+
+function calculateDamage(characterAttLvl, monsterDefLvl) {
+    let maxDamage = 100;  
+    let userMaxDamage = (characterAttLvl / 100) * maxDamage;
+    let actualDamage = (monsterDefLvl / 100) * userMaxDamage;
+    let finalDamage = Math.floor(Math.random() * actualDamage);
+
+    return finalDamage;
 }
 
