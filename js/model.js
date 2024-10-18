a5549ad2f1e5aac00c4f17f2a5e60c1d738e7cb6
casoriano03 <casoriano03@gmail.com>
2024-10-18 02:43:56 +0200

    createNewCharacterView og createNewCharacterController

--- js/model.js
+++ js/model.js
@@ -1,8 +1,9 @@
 const model = {
     // Del 1
     app: {
-        currentPage: 'homePage', //'login', 'register', 'createNew', 'map', 'shop', 'town', 'shop', 'cave', 'fight'
+        currentPage: 'createNewCharacterPage', //'login', 'register', 'createNew', 'map', 'shop', 'town', 'shop', 'cave', 'fight'
         userId: null,
+        loggedInUser: '',
     },
 
     // Del 2
@@ -26,15 +27,18 @@ const model = {
 
     // Del 3 Data
     users: [
-        { userId: null, username: null, password: null, email: null },
+        { userId: 0, username: 'Jens', password: 'jens', email: 'jens.jensen@gmail.com' },
+        { userId: 1, username: 'Lars', password: 'lars', email: 'lars.larsen@gmail.com' },
     ],
 
     characters: [
-        { userId: null, characterId: null, classId: null, areaId: 1 },
+        { userId: 0, characterId: 0, classId: 1, areaId: 1 },
+        { userId: 1, characterId: 1, classId: 2, areaId: 1 },
     ],
 
     stats: [
-        { userId: null, characterId: null, hp: null, level: null, atk: null, def: null, spd: null },
+        { userId: 0, characterId: 0, hp: 100, level: 0, atk: 20, def: 30, spd: 10 },
+        { userId: 1, characterId: 1, hp: 60, level: 0, atk: 30, def: 10, spd: 20 },
     ],
 
     inventories: [
@@ -42,31 +46,31 @@ const model = {
     ],
 
     equippedItems: [
-        { userId: null, characterId: null, weapon: null, armor: null, boots: null },
+        { userId: null, characterId: null, weaponId: null, armorId: null, bootsId: null },
     ],
 
     classes: [
-        { id: 1, class: 'Warrior', description: 'blah blah blah', picture: 'picture', hp: 100, level: 0, atk: 20, def: 30, spd: 10 },
-        { id: 2, class: 'Wizard', description: 'blah blah blah', picture: 'picture', hp: 60, level: 0, atk: 30, def: 10, spd: 20 },
-        { id: 3, class: 'Hunter', description: 'blah blah blah', picture: 'picture', hp: 80, level: 0, atk: 10, def: 20, spd: 30 },
+        { id: 1, class: 'Warrior', description: 'blah blah blah', picture: '🧑‍🚒', hp: 100, level: 0, atk: 20, def: 30, spd: 10 },
+        { id: 2, class: 'Wizard', description: 'blah blah blah', picture:'🧙‍♂️', hp: 60, level: 0, atk: 30, def: 10, spd: 20 },
+        { id: 3, class: 'Hunter', description: 'blah blah blah', picture: '🥷', hp: 80, level: 0, atk: 10, def: 20, spd: 30 },
     ],
 
     weapons: [
-        { id: 1, name: 'Old sword', description: '+5 atk', atk: 5 },
-        { id: 2, name: 'New sword', description: '+15 atk', atk: 15 },
-        { id: 3, name: 'Old staff', description: '+5 atk', atk: 5 },
-        { id: 4, name: 'New staff', description: '+15 atk', atk: 15 },
-        { id: 5, name: 'Old bow', description: '+5 atk', atk: 5 },
-        { id: 6, name: 'New bow', description: '+15 atk', atk: 15 },
+        { id: 1, class: 'Warrior', name: 'Old sword', description: '+5 atk', atk: 5 },
+        { id: 2, class: 'Warrior', name: 'New sword', description: '+15 atk', atk: 15 },
+        { id: 3, class: 'Wizard', name: 'Old staff', description: '+5 atk', atk: 5 },
+        { id: 4, class: 'Wizard', name: 'New staff', description: '+15 atk', atk: 15 },
+        { id: 5, class: 'Hunter', name: 'Old bow', description: '+5 atk', atk: 5 },
+        { id: 6, class: 'Hunter', name: 'New bow', description: '+15 atk', atk: 15 },
     ],
 
     armors: [
-        { id: 1, name: 'Old chainmail armor', description: '+5 def', def: 5 },
-        { id: 2, name: 'New chainmail armor', description: '+15 def', def: 15 },
-        { id: 3, name: 'Old cloak', description: '+5 def', def: 5 },
-        { id: 4, name: 'New cloak', description: '+15 def', def: 15 },
-        { id: 5, name: 'Old leather armor', description: '+5 def', def: 5 },
-        { id: 6, name: 'New leather armor', description: '+15 def', def: 15 },
+        { id: 1, class: 'Warrior', name: 'Old chainmail armor', description: '+5 def', def: 5 },
+        { id: 2, class: 'Warrior', name: 'New chainmail armor', description: '+15 def', def: 15 },
+        { id: 3, class: 'Wizard', name: 'Old cloak', description: '+5 def', def: 5 },
+        { id: 4, class: 'Wizard', name: 'New cloak', description: '+15 def', def: 15 },
+        { id: 5, class: 'Hunter', name: 'Old leather armor', description: '+5 def', def: 5 },
+        { id: 6, class: 'Hunter', name: 'New leather armor', description: '+15 def', def: 15 },
     ],
 
     boots: [
