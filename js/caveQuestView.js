8371c3f8e7a16a710fdeb45a70e82248e5551649
IverHK <iverhk@gmail.com>
2024-10-22 10:07:22 +0200

    ok

---
+++ js/caveQuestView.js
@@ -0,0 +1,10 @@
+function caveQuestView() {
+    document.getElementById("sceneDiv").innerHTML = /*HTML*/ `
+    <div class="caveQuestPage">
+		<div> boss </div>
+		<div> wall </div>
+		<div> monster </div>
+		<div> </div>
+    </div>
+    `;
+}
