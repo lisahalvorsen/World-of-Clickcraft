ebd3e3e1d97e3af61a0f03f3247f56be488ee303
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-10-22 14:33:38 +0200

    Lagt til shop i modellen

--- js/shopView.js
+++ js/shopView.js
@@ -1,3 +1,20 @@
 function shopView() {
+    document.getElementById('app').innerHTML = /*HTML*/ `  
+        <div>
+            <h1>Shop</h1>
+            <div>
+                <button>Equipment</button>
+                <button>Clothing</button>
+                <button>Pets</button>
+                <button>Weapons</button>
+            </div>
+            <div>${drawShopItemsHtml()}</div>
+        </div>
+    `;
+}
 
+function drawShopItemsHtml() {
+    for (const item of model.shop) {
+        console.log(item);
+    }
 }
