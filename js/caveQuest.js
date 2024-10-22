88b9a8f3022efd549bc4c7e5431896548f14e6cd
IverHK <iverhk@gmail.com>
2024-10-22 09:35:17 +0200

    Ok

--- js/caveQuest.js
+++ js/caveQuest.js
@@ -1,40 +1,73 @@
-function caveQuest(){
+let characterStats = model.stats.find(stat => stat.characterId === model.app.loggedInCharacterId);
+let characterCurrentHp = characterStats ? characterStats.currenthp : null;
+let characterAtk = characterStats.atk;
+let characterDef = characterStats.def;
 
+let caveMonsterStats = model.caveQuest[1];
+let caveMonsterCurrentHp = caveMonsterStats.currentHp;
+let caveMonsterAtk = caveMonsterStats.atk;
+let caveMonsterDef = caveMonsterStats.def;
+let caveMonsterPresent = true;
+let caveBossPresent = false;
+let stoneWallPresent = true;
+let stoneCount = 8;
+
+
+function caveQuest(){
+	// currentMission = model.caveQuest.name;
+	// currentQuestStep = model.caveQuest.currentMission;
+	// caveMonsterPresent = true;
+	if (caveMonsterPresent) {
+	}
+}
+
+function attackBossMonster(){
 
 }
 
 function attackCaveMonster(){
-	let characterStats = model.stats.find(stat => stat.characterId === model.app.loggedInCharacterId);
-	let characterCurrentHp = characterStats ? characterStats.currenthp : null;
-	let characterAtk = characterStats.atk;
-	let characterDef = characterStats.def;
-	
-	let caveMonsterStats = model.caveQuest[1];
-	let caveMonsterCurrentHp = caveMonsterStats.currentHp;
-	let caveMonsterAtk = caveMonsterStats.atk;
-	let caveMonsterDef = caveMonsterStats.def;
+	if (characterCurrentHp > 0 && caveMonsterCurrentHp > 0){
+		let caveMonsterDamageTaken = calculateDamage(characterAtk, caveMonsterDef);
+		let characterDamageTaken = calculateDamage(caveMonsterAtk, characterDef);
+		let monsterRemainingHp = caveMonsterCurrentHp - caveMonsterDamageTaken;
+		let characterRemainingHp = characterCurrentHp - characterDamageTaken;
+		characterCurrentHp = characterRemainingHp;
+		caveMonsterCurrentHp = monsterRemainingHp;
 
-	let caveMonsterDamageTaken = calculateDamage(characterAtk, caveMonsterDef);
-	let characterDamageTaken = calculateDamage(caveMonsterAtk, characterDef);
-	let monsterRemainingHp = caveMonsterCurrentHp - caveMonsterDamageTaken;
-	let characterRemainingHp = characterCurrentHp - characterDamageTaken;
+		if (characterCurrentHp <= 0) {
+			console.log(`Oops! You are dead!`);
+			goToGamePage();
+		} else if (caveMonsterCurrentHp <=0) {
+			//caveMonsterPresent = false;
 
-	console.log(`damage taken on cave monster ${caveMonsterDamageTaken}`)
-	console.log(`damage recived from cave monster ${characterDamageTaken}`)
-		
-	console.log(`Your current hitpoints are ${characterRemainingHp}`)
-	console.log(`Cave monster has ${monsterRemainingHp} hitpoints remaining`)
+			// for the correct user V
+			// model.stats.money+= 100;
+			// model.inventory.key = true
+			console.log(`Monster successfully slayed, continue`);
+		} else {
+			console.log(`damage taken on cave monster ${caveMonsterDamageTaken}`)
+			console.log(`damage recived from cave monster ${characterDamageTaken}`)
+				
+			console.log(`Your current hitpoints are ${characterCurrentHp}`)
+			console.log(`Cave monster has ${caveMonsterCurrentHp} hitpoints remaining`)
+		}
+	} 
+}
 
-	// sette inn if statement for å se om placeholder hp er gitt og dermed minke 
-	// mellomlarget hp hver gang slik at den fortsetter med lagret hp
-	
+function getThroughStones(){
+	stoneCount--;
+	if (stoneCount <= 0){
+		stoneWallPresent = false; 
+		doorPresent = true;
+	}
+}
 
-	// while (characterCurrentHp <= 0 || caveMonsterCurrentHp <= 0) {
-	// 	console.log(`Damage taken on cave monster ${calculateDamage(characterAtk, caveMonsterDef)}`)
-	// 	console.log(`Damage recived from cave monster ${calculateDamage(caveMonsterAtk, characterDef)}`)
-	// 	console.log(`Your current hitpoints are ${characterCurrentHp}`)
-	// 	console.log(`Cave monster has ${caveMonsterCurrentHp} hitpoints remaining`)
-	// }
+function getThroughDoor(){
+	//for correct character
+	//if (model.inventories.key == clicked?){
+		doorPresent = false;
+		caveBossPresent = true;	
+	//}
 }
 
 function calculateDamage(atkLvl, defLvl) {
