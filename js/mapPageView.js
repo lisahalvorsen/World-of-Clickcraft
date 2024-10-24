fa5c5357ee539e94f4fe463f349a00f79c37ee43
Faisal-hameed82 <faisalfrontend@gmail.com>
2024-10-24 14:07:11 +0200

    background music er lagt til

--- js/mapPageView.js
+++ js/mapPageView.js
@@ -1,18 +1,17 @@
 function mapPageView() {
     return /*HTML*/ `<div class='mapPage'>
+       <!-- Add audio element for background music -->
+       <audio id="backgroundMusic" autoplay loop>
+       <source src="../music/background-music.mp3" type="audio/mpeg">
+       Your browser does not support the audio element.
+   </audio>
         <!-- top -->
-        
-
-            <!-- mapPage__top__map -->
-            
+            <!-- mapPage__top__map --> 
             <div class='mapPage__top__map'>
-
-               
                  <!-- mapPage__top__images -->
 
                 <div class='mapPage__top__map__row'>
                 
-
                 <div class='mapPage__top__map__col'><img class='water_img' src="../images/water.png"></div>
                     <div class='mapPage__top__map__col'>
                         <div class='mountainIsland' onclick='goToMountainIsland()'>
