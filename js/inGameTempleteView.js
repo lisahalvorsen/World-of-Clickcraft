2dedb0c706f6d54b7102cd99b98afc2e3bc322cc
Faisal-hameed82 <faisalfrontend@gmail.com>
2024-10-25 10:26:04 +0200

    små endringer i css

--- js/inGameTempleteView.js
+++ js/inGameTempleteView.js
@@ -28,7 +28,7 @@ function gameTemplateView() {
         </div>
         <div class='headerBtn'><button onclick='logOutButton()'>Log out</button></div>
         <div class='headerChangeChar' onclick='goToCharacterSelectionPage()' >
-        🔁Change Character 
+        <p>🔁Change Character </p>
         </div>
     </header>
      <div class='sceneDiv'>
