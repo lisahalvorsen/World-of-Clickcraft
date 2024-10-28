359170a2e7a790d9b97396e596c4c02ca25f8860
Lisa Halvorsen <lisa.w.halvorsen@gmail.com>
2024-10-28 11:15:12 +0100

    Fikset shop

--- js/shopView.js
+++ js/shopView.js
@@ -1,32 +1,33 @@
-function shopView() {
+function shopView(items = model.shop) {
     document.getElementById('app').innerHTML = /*HTML*/ `  
         <div>
             <h1>Shop</h1>
             <div>🪙: ${getUserMoney()}</div>
             <div>
-                <button onclick="drawShopItemsHtml('Equipment')">Equipment</button>
-                <button onclick="drawShopItemsHtml('Clothing')">Clothing</button>
-                <button onclick="drawShopItemsHtml('Pets')">Pets</button>
-                <button onclick="drawShopItemsHtml('Weapons')">Weapons</button>
+                <button onclick="getShopInventory('Equipment')">Equipment</button>
+                <button onclick="getShopInventory('Clothing')">Clothing</button>
+                <button onclick="getShopInventory('Pets')">Pets</button>
+                <button onclick="getShopInventory('Weapons')">Weapons</button>
             </div>
-            <div id="items"></div>
-            <button onclick="goBackToMap()">Exit</button>
+            ${drawAllShopItemsHtml(items)}
+            <br>
+            <button onclick="goBackToMap()">Exit shop</button>
         </div>
     `;
 }
 
-function drawShopItemsHtml(category = null) {
+function drawAllShopItemsHtml(items) {
     let html = '';
 
-    for (const item of model.shop) {
-        if (!category || item.category === category) {
-            html += /*HTML*/ `                 
-            <img class="shopItems" src="${item.picture}" />
-            <div class="itemName">${item.name}</div>                 
-            <div>$ ${item.price}</div>                  
-            <button onclick="buyItem()">Buy</button>             
-            `;
-        }
+    for (const item of items) {
+        html += /*HTML*/ `
+            <div>
+                <img class="shopItems" src="${item.picture}" />
+                <div class="itemName">${item.name}</div>
+                <div>$ ${item.price}</div>
+                <button onclick="buyItem()">Buy</button>
+            </div>
+        `;
     }
-    document.getElementById('items').innerHTML = html; // endre på denne (?) + legge til bilder + CSS
+    return html;
 }
