995f06f69d022fe82254b3e638f75a5f5028b4a8
IverHK <iverhk@gmail.com>
2024-10-28 09:20:08 +0100

    ok

--- js/caveQuestController.js
+++ js/caveQuestController.js
@@ -1,54 +1,75 @@
 
 let keySelected = false;
+//common
+function findCharacterMessageLog(userId, characterId) {
+    return model.messageLog.find(msg => msg.userId === userId && msg.characterId === characterId);
+}
+function findCharacterInventory(userId, characterId) {
+    return model.inventories.find(invent => invent.userId === userId && invent.characterId === characterId);
+}
+function findCharacterCaveQuest(userId, characterId) {
+    return model.caveQuest.filter(id => id.userId === userId && id.characterId === characterId);
+}
+function findCharacterStats(userId, characterId) {
+    return model.stats.find(userStat => userStat.userId === userId && userStat.characterId === characterId);
+}
+
+
+//legge til caveQuest[0].currentQuestStep i modellen
+
 // koble til inventory
 // legge til health potion mulighet mid game.. endrer characterCurrentHp
 // koble quest til hver enkelt bruker
 // få loot fra quest/kill
 
 function attackBossMonster(){
-	model.app.currentCharacterInfo.messageLog.push('Kill that thang!')
+	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
+	messageLog.text.push('Kill that thang!');
+
 	gameView = caveQuestView();
 	gameTemplateView();
 }
 
 function attackCaveMonster(){
-	let caveMonsterStats = model.caveQuest[1];
-	let currentCharacter = model.app.currentCharacterInfo;
+	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const caveQuest = findCharacterCaveQuest(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const characterStats = findCharacterStats (model.app.loggedInUser, model.app.loggedInCharacterId)
+	const characterInventory = findCharacterInventory (model.app.loggedInUser, model.app.loggedInCharacterId)
+	let caveMonsterStats = caveQuest[1];
 
-	if (model.app.currentCharacterInfo.currenthp > 0 && caveMonsterStats.currentHp > 0){
-		let caveMonsterDamageTaken = calculateDamage(currentCharacter.atk, caveMonsterStats.def);
-		let characterDamageTaken = calculateDamage(caveMonsterStats.atk, currentCharacter.def);
+	if (characterStats.currenthp > 0 && caveMonsterStats.currentHp > 0){
+		let caveMonsterDamageTaken = calculateDamage(characterStats.atk, caveMonsterStats.def);
+		let characterDamageTaken = calculateDamage(caveMonsterStats.atk, characterStats.def);
 		let monsterRemainingHp = caveMonsterStats.currentHp - caveMonsterDamageTaken;
-		let characterRemainingHp = model.app.currentCharacterInfo.currenthp - characterDamageTaken;
-		model.app.currentCharacterInfo.currenthp = characterRemainingHp;
+		let characterRemainingHp = characterStats.currenthp - characterDamageTaken;
+		characterStats.currenthp = characterRemainingHp;
 		caveMonsterStats.currentHp = monsterRemainingHp;
-		gameTemplateView();
 
-		if (model.app.currentCharacterInfo.currenthp <= 0) {
-			model.app.currentCharacterInfo.messageLog.push(`Oops! You are dead!`);
-			model.app.currentCharacterInfo.currenthp = model.app.currentCharacterInfo.hp;
+		if (characterStats.currenthp <= 0) {
+			messageLog.text.push(`Oops! You are dead!`);
+			characterStats.currenthp = characterStats.hp;
 
 		} else if (caveMonsterStats.currentHp <=0) {
 			model.caveQuest[1].caveMonsterPresent = false;
-			model.app.currentCharacterInfo.currentQuestStep = 'Make your way though the obstacle';
-			model.app.currentCharacterInfo.xp += 100;
-			model.app.currentCharacterInfo.level ++;
-			model.app.currentCharacterInfo.atk ++;
-			model.app.currentCharacterInfo.def ++;
-			model.app.currentCharacterInfo.spd ++;
-			model.app.currentCharacterInfo.money += 100;
-			// or drop money, key and health potions on floor first
+			caveQuest[0].currentQuestStep = 'Make your way though the obstacle';
+			characterStats.xp += 100;
+			characterStats.level ++;
+			characterStats.atk ++;
+			characterStats.def ++;
+			characterStats.spd ++;
 
-			// take info from currentCharacterInfo instead below
+			//characterInventory
+			// or drop money, key and health potions on floor first
+			characterStats.money += 100;
 			model.inventories[0].hasKey = true;
 			model.caveQuest[0].progress++;
-			model.app.currentCharacterInfo.messageLog.push(`Monster successfully slayed, continue`);
+			messageLog.text.push(`Monster successfully slayed, continue`);
 
 		} else {
-			model.app.currentCharacterInfo.messageLog.push(`Damage taken on cave monster: ${caveMonsterDamageTaken}.`)
-			model.app.currentCharacterInfo.messageLog.push(`Damage recived from cave monster: ${characterDamageTaken}.`)
-			model.app.currentCharacterInfo.messageLog.push(`Your current hitpoints are ${model.app.currentCharacterInfo.currenthp}.`)
-			model.app.currentCharacterInfo.messageLog.push(`Cave monster has ${caveMonsterStats.currentHp} hitpoints remaining.`)
+			messageLog.text.push(`Damage taken on cave monster: ${caveMonsterDamageTaken}.`)
+			messageLog.text.push(`Damage recived from cave monster: ${characterDamageTaken}.`)
+			messageLog.text.push(`Your current hitpoints are ${characterStats.currenthp}.`)
+			messageLog.text.push(`Cave monster has ${caveMonsterStats.currentHp} hitpoints remaining.`)
 		}
 	} 
 	gameView = caveQuestView();
@@ -56,43 +77,53 @@ function attackCaveMonster(){
 }
 
 function getThroughStones(){
-		// take info from currentCharacterInfo instead below
-			if (model.caveQuest[0].progress === 1 && model.caveQuest[0].stoneWallPresent) {
-		model.caveQuest[0].stoneCount--;
-		model.app.currentCharacterInfo.messageLog.push('You remove one stone...');
-		if (model.caveQuest[0].stoneCount == 0){
-			model.caveQuest[0].stoneWallPresent = false; 
-			model.caveQuest[0].doorPresent = true;
-			model.app.currentCharacterInfo.messageLog.push('A door appear behind the stones.');
-			model.app.currentCharacterInfo.xp += 50;
+	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const caveQuest = findCharacterCaveQuest(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const characterStats = findCharacterStats (model.app.loggedInUser, model.app.loggedInCharacterId)
+
+		if (caveQuest[0].progress === 1 && caveQuest[0].stoneWallPresent) {
+			caveQuest[0].stoneCount--;
+			messageLog.text.push('You remove one stone...');
+		if (caveQuest[0].stoneCount == 0){
+			caveQuest[0].stoneWallPresent = false; 
+			caveQuest[0].doorPresent = true;
+			messageLog.text.push('A door appear behind the stones.');
+			characterStats.xp += 50;
 
 		}
 	} else {
-		model.app.currentCharacterInfo.messageLog.push('You must slay the monster before approaching the door.');
+		messageLog.text.push('You must slay the monster before approaching the door.');
 	}
 	gameView = caveQuestView();
 	gameTemplateView();
 }
 
 function getThroughDoor(){
+	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const caveQuest = findCharacterCaveQuest(model.app.loggedInUser, model.app.loggedInCharacterId);
+	const characterStats = findCharacterStats (model.app.loggedInUser, model.app.loggedInCharacterId)
+	const characterInventory = findCharacterInventory (model.app.loggedInUser, model.app.loggedInCharacterId)
+
 	//for correct character
-	if (model.inventories[0].hasKey == true && keySelected == true){
-		model.caveQuest[0].doorPresent = false;
-		model.caveQuest[2].caveBossPresent = true;	
-		model.app.currentCharacterInfo.messageLog.push('You unlock the door and enter a room where you see a HUGE creature!');
-		model.app.currentCharacterInfo.currentQuestStep = 'Kill the Cave Boss';
-		model.app.currentCharacterInfo.xp += 50;
-		model.app.currentCharacterInfo.level ++;
+	if (characterInventory.hasKey == true && keySelected == true){
+		caveQuest[0].doorPresent = false;
+		caveQuest[2].caveBossPresent = true;	
+		messageLog.text.push('You unlock the door and enter a room where you see a HUGE creature!');
+		caveQuest.currentQuestStep = 'Kill the Cave Boss';
+		characterStats.xp += 50;
+		characterStats.level ++;
 	} else {
-		model.app.currentCharacterInfo.messageLog.push('Perhaps I can use the key the monster dropped..?');
+		messageLog.text.push('Perhaps I can use the key the monster dropped..?');
 	}
 	gameView = caveQuestView();
 	gameTemplateView();
 }
 
 function selectKey(){
+	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
+
 	keySelected = !keySelected; 
-    model.app.currentCharacterInfo.messageLog.push(keySelected ? 'Key selected' : 'Key unselected');
+	messageLog.text.push(keySelected ? 'Key selected' : 'Key unselected');
 	gameView = caveQuestView();
 	gameTemplateView();
 }
--- js/common.js
+++ js/common.js
@@ -75,8 +75,6 @@ function goToTownIsland() {
     gameTemplateView();
 }
 function goToCaveIsland() {
-    model.app.currentCharacterInfo.currentQuest = model.caveQuest[0].name;
-    model.app.currentCharacterInfo.currentQuestStep = model.caveQuest[0].currentQuestStep;
     gameView = caveQuestView();
     gameTemplateView();
 }
