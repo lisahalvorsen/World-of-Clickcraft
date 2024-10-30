eb3dd91b9b3e76826a294d39fa549ebdb20e102f
casoriano03 <casoriano03@gmail.com>
2024-10-30 14:23:12 +0100

    town elder - functionality update

--- js/townController.js
+++ js/townController.js
@@ -6,7 +6,7 @@ function generateMission() {
     To free our land, you must defeat the Demon Lord and their formidable army. 
     You can track your quest's progress on the Mission Board below. 
     To reach the Demon Lord, you must first conquer the Army Generals who guard their territories: the Cave, the Desert, the Forest, and the Mountain. Good luck, hero!`;
-    let statusMessage = `Town Elder: Heal up ${characterInfo.name}! You still have monsters to defeat: ${characterInfo.currentQuest} - ${characterInfo.currentQuestStep}.`;
+    let statusMessage = `Town Elder: Heal up ${characterInfo.name}! You still have monsters to defeat. ${characterInfo.currentQuest} - ${characterInfo.currentQuestStep}.`;
     let messageLog = findCharacterMessageLog(userId, charactedId)
     let questStatus = findQuestStatus(userId, charactedId)
     let newQuestStatus = {userId:userId, characterId:charactedId, caveQuest:false, desertQuest: false, forestQuest: false, mountainQuest: false, bossQuest: false}
@@ -19,4 +19,12 @@ function generateMission() {
         model.questStatus.push(newQuestStatus)
     }
     gameTemplateView()
+}
+
+function questUpdater(areaCompleted, nextArea) {
+    let userId = model.app.loggedInUser
+    let charactedId = model.app.loggedInCharacterId
+    let messageLog = findCharacterMessageLog(userId, charactedId)
+    let message = `Congratulations in completeing the ${areaCompleted} quest. Go to Town and heal up then prepare for the ${nextArea} quest.`
+    messageLog.text.push(message)
 }
