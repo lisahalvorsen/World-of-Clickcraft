f2e7f9d4ebe8a046dbf1cfb00d9d5cbb5cea9856
IverHK <iverhk@gmail.com>
2024-10-31 22:39:02 +0100

    antall i inventory fungerer

--- js/inventoryController.js
+++ js/inventoryController.js
@@ -4,16 +4,16 @@ function useHealthPotion() {
 	const messageLog = findCharacterMessageLog(model.app.loggedInUser, model.app.loggedInCharacterId);
 	const healthPotion = characterInventory.items.find(item => item.name === 'Health potion');
 
-	if (healthPotion.count <= 0) {
+	if (healthPotion.quantity <= 0) {
 		messageLog.text.push('Oh no! You dont have any more health potions!')
 	} else {
 		if (characterStats.currenthp + 50 >= characterStats.hp) {
 			characterStats.currenthp = characterStats.hp;
-			healthPotion.count--;
+			healthPotion.quantity--;
 			messageLog.text.push('You drink a health potion! You have full health')
 		} else {
 			characterStats.currenthp += 50;
-			healthPotion.count--;
+			healthPotion.quantity--;
 			messageLog.text.push('You drink a health potion! Your health have increased by 50!')
 		}
 	}
